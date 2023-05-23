12b6bbf6354c527075a577a815e8a0d284ae55a5
Talha SARI <sarit17@itu.edu.tr>
2023-05-23 23:57:52 +0300

    Improved user experience by adding restrictions to buttons

--- smile-detection.js
+++ smile-detection.js
@@ -18,8 +18,14 @@ let isSmiling2 = false;
 let intervalStarted = false;
 // Add a variable to keep track of whether the game is over
 let gameOver = false;
-// Add a startGame function
+// Declare a timeoutID variable
+let timeoutID;
+// Declare a gameStarted variable
+let gameStarted = false;
+
 function startGame() {
+  // Enable restart-button
+  document.getElementById("restart-button").disabled = false;
   // Reset the game state
   smileCounter1 = 0;
   smileCounter2 = 0;
@@ -29,9 +35,15 @@ function startGame() {
   smileCounterElement2.textContent = `Person 2 Smiles: ${smileCounter2}`;
   // Hide the pop-up
   popup.style.display = "none";
-
+  
+  // Set gameStarted to true
+  gameStarted = true;
+  
+  // Disable start-button
+  document.getElementById("start-button").disabled = true;
+  
   // Add a setTimeout function that runs after 10 seconds
-  setTimeout(() => {
+  timeoutID = setTimeout(() => {
     // Display the pop-up
     popup.style.display = "block";
     // Determine the winner
@@ -47,110 +59,105 @@ function startGame() {
     winnerElement.textContent = `Winner: ${winner}`;
     // Set the gameOver variable to true
     gameOver = true;
-  }, 10000);
+    // Disable restart-button
+    document.getElementById("restart-button").disabled = true;
+  },10000);
 }
 
 async function startSmileDetection() {
-  try {
-    // Load the models
-    await faceapi.nets.ssdMobilenetv1.loadFromUri("./weights");
-    await faceapi.nets.faceLandmark68Net.loadFromUri("./weights");
-    await faceapi.nets.faceExpressionNet.loadFromUri("./weights");
+ try {
+   await faceapi.nets.ssdMobilenetv1.loadFromUri("./weights");
+   await faceapi.nets.faceLandmark68Net.loadFromUri("./weights");
+   await faceapi.nets.faceExpressionNet.loadFromUri("./weights");
 
-    // Start the video stream
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then((stream) => {
-        video.srcObject = stream;
-      })
-      .catch((error) => {
-        console.error("Could not access the webcam:", error);
-      });
+   navigator.mediaDevices
+     .getUserMedia({ video: true })
+     .then((stream) => {
+       video.srcObject = stream;
+     })
+     .catch((error) => {
+       console.error("Could not access the webcam:", error);
+     });
 
-    video.addEventListener("play", () => {
-      const canvas = faceapi.createCanvasFromMedia(video);
-      document.body.append(canvas);
-      const displaySize = { width: video.width, height: video.height };
-      faceapi.matchDimensions(canvas, displaySize);
+   video.addEventListener("play", () => {
+     const canvas = faceapi.createCanvasFromMedia(video);
+     document.body.append(canvas);
+     const displaySize = { width: video.width, height: video.height };
+     faceapi.matchDimensions(canvas, displaySize);
 
-      if (!intervalStarted) {
-        intervalStarted = true;
-        setInterval(async () => {
-          if (!video.paused && !video.ended) {
-            const detections = await faceapi
-              .detectAllFaces(video, new faceapi.SsdMobilenetv1Options())
-              .withFaceLandmarks()
-              .withFaceExpressions();
-            const resizedDetections = faceapi.resizeResults(
-              detections,
-              displaySize
-            );
-            canvas
-              .getContext("2d")
-              .clearRect(0, 0, canvas.width, canvas.height);
-            faceapi.draw.drawDetections(canvas, resizedDetections);
-            faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+     if (!intervalStarted) {
+       intervalStarted = true;
+       setInterval(async () => {
+         if (!video.paused && !video.ended) {
+           const detections = await faceapi
+             .detectAllFaces(video, new faceapi.SsdMobilenetv1Options())
+             .withFaceLandmarks()
+             .withFaceExpressions();
+           const resizedDetections = faceapi.resizeResults(
+             detections,
+             displaySize
+           );
+           canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+           faceapi.draw.drawDetections(canvas, resizedDetections);
+           faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 
-            if (resizedDetections.length >= 1) {
-              if (resizedDetections[0].expressions.happy > 0.7) {
-                consecutiveSmiles1++;
-                if (
-                  consecutiveSmiles1 >= MIN_CONSECUTIVE_FRAMES &&
-                  !isSmiling1
-                ) {
-                  smileCounter1++;
-                  isSmiling1 = true;
-                  consecutiveSmiles1 = 0;
-                }
-              } else {
-                isSmiling1 = false;
-                consecutiveSmiles1 = 0;
-              }
-              smileCounterElement1.textContent = `Person 1 Smiles: ${smileCounter1}`;
-            }
-            if (resizedDetections.length >= 2) {
-              if (resizedDetections[1].expressions.happy > 0.7) {
-                consecutiveSmiles2++;
-                if (
-                  consecutiveSmiles2 >= MIN_CONSECUTIVE_FRAMES &&
-                  !isSmiling2
-                ) {
-                  smileCounter2++;
-                  isSmiling2 = true;
-                  consecutiveSmiles2 = 0;
-                }
-              } else {
-                isSmiling2 = false;
-                consecutiveSmiles2 = 0;
-              }
-              smileCounterElement2.textContent = `Person 2 Smiles: ${smileCounter2}`;
-            }
-          }
-        }, 100);
-      }
-
-      // Call startGame when starting smile detection
-      startGame();
-    });
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
+           if (resizedDetections.length >= 1) {
+             if (resizedDetections[0].expressions.happy > 0.7) {
+               consecutiveSmiles1++;
+               if (
+                 consecutiveSmiles1 >= MIN_CONSECUTIVE_FRAMES &&
+                 !isSmiling1
+               ) {
+                 smileCounter1++;
+                 isSmiling1 = true;
+                 consecutiveSmiles1 = 0;
+               }
+             } else {
+               isSmiling1 = false;
+               consecutiveSmiles1 = 0;
+             }
+             smileCounterElement1.textContent = `Person 1 Smiles: ${smileCounter1}`;
+           }
+           if (resizedDetections.length >= 2) {
+             if (resizedDetections[1].expressions.happy > 0.7) {
+               consecutiveSmiles2++;
+               if (
+                 consecutiveSmiles2 >= MIN_CONSECUTIVE_FRAMES &&
+                 !isSmiling2
+               ) {
+                 smileCounter2++;
+                 isSmiling2 = true;
+                 consecutiveSmiles2 = 0;
+               }
+             } else {
+               isSmiling2 = false;
+               consecutiveSmiles2 = 0;
+             }
+             smileCounterElement2.textContent = `Person 2 Smiles: ${smileCounter2}`;
+           }
+         }
+       },100);
+     }
+   });
+ } catch (error) {
+   console.error("An error occurred:", error);
+ }
 }
 
-document
-  .getElementById("start-button")
-  .addEventListener("click", startSmileDetection);
+// Add event listener for start-button 
+document.getElementById("start-button").addEventListener("click", startGame);
 
-document.getElementById("stop-button").addEventListener("click", () => {
-  video.pause();
+// Update restart-button event listener to only call startGame if game has started 
+document.getElementById("restart-button").addEventListener("click", () => { 
+  	if (gameStarted) { 
+  		clearTimeout(timeoutID); 
+  		startGame(); 
+  	} 
 });
 
-document.getElementById("reset-button").addEventListener("click", () => {
-  smileCounter1 = 0;
-  smileCounter2 = 0;
-  smileCounterElement1.textContent = `Person 1 Smiles: ${smileCounter1}`;
-  smileCounterElement2.textContent = `Person 2 Smiles: ${smileCounter2}`;
+// Update play-again event listener to only call startGame if game is over 
+playAgainButton.addEventListener('click', () => { 
+  	if (gameOver) { 
+  		startGame(); 
+  	} 
 });
-
-// Call startGame when playAgainButton is clicked
-playAgainButton.addEventListener("click", startGame);
